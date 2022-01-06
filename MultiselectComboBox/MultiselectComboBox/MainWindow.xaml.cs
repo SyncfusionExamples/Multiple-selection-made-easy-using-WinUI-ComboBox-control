83bbd242ff7d940c0a3719b6354783a43653d046
Jegan R <jeganr@syncfusion.com>
2022-01-06 18:17:09 +0530

    Allow adding custom email
    Allow adding custom email

--- MultiselectComboBox/MultiselectComboBox/MainWindow.xaml.cs
+++ MultiselectComboBox/MultiselectComboBox/MainWindow.xaml.cs
@@ -5,13 +5,16 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Syncfusion.UI.Xaml.Editors;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +29,44 @@ namespace MultiselectComboBox
         public MainWindow()
         {
             this.InitializeComponent();
+        }
+
+        private async void OnInputSubmitted(object sender, ComboBoxInputSubmittedEventArgs e)
+        {
+            var emailString = e.Text;
+            bool isEmail = Regex.IsMatch(emailString, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+
+            // If given input is a valid email format
+            if (isEmail)
+            {
+                // Create new person for selection
+                Person newPerson = new Person();
+                newPerson.EmailID = emailString;
+                int atIndex = emailString.LastIndexOf('@');
+                int dotIndex = emailString.IndexOf('.');
+                string name = emailString.Substring(0, atIndex);
+                if (name.Contains('.'))
+                {
+                    newPerson.FirstName = name.Substring(0, dotIndex);
+                    newPerson.LastName = name.Substring(dotIndex + 1);
+                }
+                else
+                {
+                    newPerson.FirstName = name;
+                }
+                e.Item = newPerson;
+            }
+
+            // If email is id invalid, show error dialog.
+            else
+            {
+                var dialog = new ContentDialog();
+                dialog.CloseButtonText = "Close";
+                dialog.Content = "Invalid email id!";
+                dialog.Title = "Error";
+                dialog.XamlRoot = this.Content.XamlRoot;
+                await dialog.ShowAsync();
+            }
         }
     }
 }
