b2cc2ed1253c137d3c6d26b2e16225b0378c9764
Jegan R <jeganr@syncfusion.com>
2022-01-11 10:20:42 +0530

    Added EventTriggerBehavior
    Added EventTriggerBehavior

--- MultiselectComboBox/MultiselectComboBox/MainWindow.xaml.cs
+++ MultiselectComboBox/MultiselectComboBox/MainWindow.xaml.cs
@@ -30,43 +30,5 @@ namespace MultiselectComboBox
         {
             this.InitializeComponent();
         }
-
-        private async void OnInputSubmitted(object sender, ComboBoxInputSubmittedEventArgs e)
-        {
-            var emailString = e.Text;
-            bool isEmail = Regex.IsMatch(emailString, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
-
-            // If given input is a valid email format
-            if (isEmail)
-            {
-                // Create new person for selection
-                Person newPerson = new Person();
-                newPerson.EmailID = emailString;
-                int atIndex = emailString.LastIndexOf('@');
-                int dotIndex = emailString.IndexOf('.');
-                string name = emailString.Substring(0, atIndex);
-                if (name.Contains('.'))
-                {
-                    newPerson.FirstName = name.Substring(0, dotIndex);
-                    newPerson.LastName = name.Substring(dotIndex + 1);
-                }
-                else
-                {
-                    newPerson.FirstName = name;
-                }
-                e.Item = newPerson;
-            }
-
-            // If email is id invalid, show error dialog.
-            else
-            {
-                var dialog = new ContentDialog();
-                dialog.CloseButtonText = "Close";
-                dialog.Content = "Invalid email id!";
-                dialog.Title = "Error";
-                dialog.XamlRoot = this.Content.XamlRoot;
-                await dialog.ShowAsync();
-            }
-        }
     }
 }
